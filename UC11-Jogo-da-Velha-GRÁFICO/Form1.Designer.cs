e6b075f9ad7f0160a989e77eba1fa091362e235a
Mateus Brebal <mateus.brebal@gmail.com>
2025-08-11 17:09:00 -0300

    Primeiro COMMIT do Projeto JOGO DA VELHA.

---
+++ UC11-Jogo-da-Velha-GRÁFICO/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace UC11_Jogo_da_Velha_GRÁFICO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::UC11_Jogo_da_Velha_GRÁFICO.Properties.Resources.Tabuleiro___JOGO_DA_VELHA1;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 84);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Impact", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(268, 275);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(88, 79);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "?";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Impact", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(157, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 79);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "?";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Impact", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(47, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 79);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "?";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Impact", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(268, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 79);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "?";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Impact", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(157, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 79);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "?";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Impact", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(47, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 79);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "?";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Impact", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(268, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 79);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "?";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Impact", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(157, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 79);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "?";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Impact", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(47, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 79);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 496);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Jogo da Velha";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ UC11-Jogo-da-Velha-GRÁFICO/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC11_Jogo_da_Velha_GRÁFICO
+{
+    public partial class Form1 : Form
+    {
+        string jogada = "O";
+        string acertou = "N";
+        int tentativas = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TesteJogadas()
+        {
+            // VALIDA VERTICAIS
+            if (label1.Text == label4.Text && label4.Text == label7.Text && label4.Text != "?")
+            {
+                MessageBox.Show("Jogador " + label4.Text + " ganhou!");
+                acertou = "S";
+            }
+            if (label2.Text == label5.Text && label5.Text == label8.Text && label5.Text != "?")
+            {
+                MessageBox.Show("Jogador " + label5.Text + " ganhou!");
+                acertou = "S";
+            }
+            if (label3.Text == label6.Text && label6.Text == label9.Text && label6.Text != "?")
+            {
+                MessageBox.Show("Jogador " + label6.Text + " ganhou!");
+                acertou = "S";
+            }
+
+            // VALIDA HORIZONTAIS
+            if (label1.Text == label2.Text && label2.Text == label3.Text && label2.Text != "?")
+            {
+                MessageBox.Show("Jogador " + label2.Text + " ganhou!");
+                acertou = "S";
+            }
+            if (label4.Text == label5.Text && label5.Text == label6.Text && label5.Text != "?")
+            {
+                MessageBox.Show("Jogador " + label5.Text + " ganhou!");
+                acertou = "S";
+            }
+            if (label7.Text == label8.Text && label8.Text == label9.Text && label8.Text != "?")
+            {
+                MessageBox.Show("Jogador " + label8.Text + " ganhou!");
+                acertou = "S";
+            }
+
+            // VALIDA DIAGONAIS
+            if (label1.Text == label5.Text && label5.Text == label9.Text && label5.Text != "?")
+            {
+                MessageBox.Show("Jogador " + label5.Text + " ganhou!");
+                acertou = "S";
+            }
+            if (label3.Text == label5.Text && label5.Text == label7.Text && label5.Text != "?")
+            {
+                MessageBox.Show("Jogador " + label5.Text + " ganhou!");
+                acertou = "S";
+            }
+
+            if (acertou == "N")
+            {
+                tentativas++; // tentativas = tentativas + 1;
+            } else
+            {
+                acertou = "N";
+            }
+
+            MessageBox.Show(tentativas.ToString());
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (jogada == "O")
+            {
+                label1.Text = "X";
+                jogada = "X";
+            } else
+            {
+                label1.Text = "O";
+                jogada = "O";
+            }
+            TesteJogadas();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (jogada == "O")
+            {
+                label2.Text = "X";
+                jogada = "X";
+            }
+            else
+            {
+                label2.Text = "O";
+                jogada = "O";
+            }
+            TesteJogadas();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (jogada == "O")
+            {
+                label3.Text = "X";
+                jogada = "X";
+            }
+            else
+            {
+                label3.Text = "O";
+                jogada = "O";
+            }
+            TesteJogadas();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            if (jogada == "O")
+            {
+                label4.Text = "X";
+                jogada = "X";
+            }
+            else
+            {
+                label4.Text = "O";
+                jogada = "O";
+            }
+            TesteJogadas();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            if (jogada == "O")
+            {
+                label5.Text = "X";
+                jogada = "X";
+            }
+            else
+            {
+                label5.Text = "O";
+                jogada = "O";
+            }
+            TesteJogadas();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (jogada == "O")
+            {
+                label6.Text = "X";
+                jogada = "X";
+            }
+            else
+            {
+                label6.Text = "O";
+                jogada = "O";
+            }
+            TesteJogadas();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (jogada == "O")
+            {
+                label7.Text = "X";
+                jogada = "X";
+            }
+            else
+            {
+                label7.Text = "O";
+                jogada = "O";
+            }
+            TesteJogadas();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (jogada == "O")
+            {
+                label8.Text = "X";
+                jogada = "X";
+            }
+            else
+            {
+                label8.Text = "O";
+                jogada = "O";
+            }
+            TesteJogadas();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            if (jogada == "O")
+            {
+                label9.Text = "X";
+                jogada = "X";
+            }
+            else
+            {
+                label9.Text = "O";
+                jogada = "O";
+            }
+            TesteJogadas();
+        }
+    }
+}
